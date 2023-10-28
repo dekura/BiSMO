68e4e41379851ef78135d60ef109530c4da0281f
Hongquan He <hongquanhe@HEs-Mac-Mini.local>
2023-10-28 21:51:03 +0800

    add smo random

---
+++ scripts/smorandom.sh
@@ -0,0 +1,39 @@
+python=/home/hehq/anaconda3/envs/smo/bin/python
+
+# source_list="conventional annular quasar dipole"
+source_list="annular"
+
+for type in $source_list; do
+    for i in $(seq 1 10); do
+        layout_path="/home/hehq/project/smo2.0/data/img2013/M1_test"$i".png"
+        echo "Starting random smo for: "$layout_path
+        $python src/smorandom.py task_name="sss_7" mask.layout_path=$layout_path mask.target_path=$layout_path source.source_type=$type
+    done
+done
+# for type in $source_list; do
+#     for k in $(seq 3000 1000 5000); do
+#     weight_pvb=$k
+#         for i in $(seq 1 10); do
+#             layout_path="/home/hehq/project/smo2.0/data/img2013/M1_test"$i".png"
+#             echo "Starting mo for: "$layout_path
+#             $python src/moabbe.py task_name="pvb"$weight_pvb mask.layout_path=$layout_path mask.target_path=$layout_path source.source_type=$type model.weight_pvb=$weight_pvb
+#         done
+#     done
+# done
+
+# for type in $source_list; do
+#     for i in $(seq 4 4 16); do
+#         mask_sigmoid_steepness=$i
+#         for j in $(seq 60 15 90); do
+#             resist_sigmoid_steepness=$j
+#             for k in $(seq 7000 1000 9000); do
+#                 weight_pvb=$k
+#                 for l in $(seq 1 10); do
+#                     layout_path="/home/hehq/project/smo2.0/data/img2013/M1_test"$l".png"
+#                     echo "Starting mo for: "$mask_sigmoid_steepness $resist_sigmoid_steepness $weight_pvb
+#                     $python src/moabbe.py task_name="mo_mss"$mask_sigmoid_steepness"rss"$resist_sigmoid_steepness"pvb"$weight_pvb mask.layout_path=$layout_path mask.target_path=$layout_path source.source_type=$type model.mask_sigmoid_steepness=$mask_sigmoid_steepness model.resist_sigmoid_steepness=$resist_sigmoid_steepness model.weight_pvb=$weight_pvb
+#                 done
+#             done
+#         done
+#     done
+# done
