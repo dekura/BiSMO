0fc2779cbbf8ea3aa33007e72a37be729ec2218f
Guojin Chen <cgjcuhk@gmail.com>
2023-10-28 13:30:12 -0500

    update ispd exp

---
+++ cg_ispd_d0.sh
@@ -0,0 +1,20 @@
+###
+ # @Author: Guojin Chen @ CUHK-CSE
+ # @Homepage: https://gjchen.me
+ # @Date: 2023-10-23 17:32:01
+ # @LastEditTime: 2023-10-28 13:27:48
+ # @Contact: cgjcuhk@gmail.com
+ # @Description:
+###
+python=/home/local/eda13/gc29434/miniconda3/envs/smo/bin/python
+layout_folder=/home/local/eda13/gc29434/phd/projects/SMO-DAC24-codes/data/ispd_dataset/
+
+device_id=0
+problems_type=cg
+
+for i in $(seq 45 -1 1); do
+    layout_path=$layout_folder"ispd_c"$i".png"
+    echo "Starting mo on device $device_id for: problems [$problems_type] mask $i"
+    echo $layout_path
+    $python src/bilevel.py module.device_id=$device_id problems=$problems_type mask.layout_path=$layout_path mask.target_path=$layout_path
+done
--- darts_ispd_d2.sh
+++ darts_ispd_d2.sh
@@ -2,16 +2,17 @@
  # @Author: Guojin Chen @ CUHK-CSE
  # @Homepage: https://gjchen.me
  # @Date: 2023-10-23 17:32:01
- # @LastEditTime: 2023-10-27 16:21:31
+ # @LastEditTime: 2023-10-28 13:23:56
  # @Contact: cgjcuhk@gmail.com
  # @Description:
 ###
 python=/home/local/eda13/gc29434/miniconda3/envs/smo/bin/python
-layout_folder=/home/local/eda13/gc29434/phd/projects/SMO-DAC24-codes/data/ibm_large_test/
+layout_folder=/home/local/eda13/gc29434/phd/projects/SMO-DAC24-codes/data/ispd_dataset/
 device_id=2
 
-for i in $(seq 20 -1 11); do
-    layout_path=$layout_folder"t"$i"_0_mask.png"
-    echo "Starting mo for: problems [darts] mask "$i
+for i in $(seq 45 -1 1); do
+    layout_path=$layout_folder"ispd_c"$i".png"
+    echo "Starting mo on device $device_id for: problems [darts] mask "$i
+    echo $layout_path
     $python src/bilevel.py module.device_id=$device_id mask.layout_path=$layout_path mask.target_path=$layout_path
 done
